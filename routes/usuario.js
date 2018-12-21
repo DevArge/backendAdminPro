3d9287802aebfa33ed07f33ede4891ec3badb316
Argenis <bernyargenis@yaho.com>
2018-12-21 12:54:17 -0600

    Usuario y token listo

---
+++ routes/usuario.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var app = express();
+var Usuario = require('../models/usuario');
+var bcrypt = require('bcryptjs');
+var middAutenticacion = require('../middlewares/autenticacion')
+
+app.get('/', (req, res, next)=>{
+    Usuario.find({},'nombre email img role')
+           .exec((err, usuarioDB)=>{
+                    if(err) return res.status(500).json({ok:false, mensaje: 'Error cargando usuario', errors: err})
+                    res.status(200).json({
+                        ok:true,
+                        Usuarios: usuarioDB
+                    });
+                });
+});
+
+app.post('/', middAutenticacion.verificaToken, (req, res)=>{
+    var body = req.body;
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        img: body.img,
+        role: body.role
+    });
+    usuario.save((err, usuarioDB)=>{
+        if(err) return res.status(400).json({ok:false, mensaje: 'Error al crear usuario', errors: err})
+        res.status(201).json({
+            ok:true,
+            Usuario: usuarioDB
+        });
+    });
+});
+
+app.put('/:id', middAutenticacion.verificaToken, (req, res)=>{
+    var id = req.params.id;
+    var body = req.body;
+    Usuario.findById(id, (err, usuarioDB)=>{
+        if(err) return res.status(500).json({ok:false, mensaje: 'Error al crear usuario', errors: err})
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok:false,
+                mensaje: 'El usuario no existe',
+                error: { message: 'No existe un usuario con ese ID'}
+            })
+        }
+        usuarioDB.nombre= body.nombre;
+        usuarioDB.email= body.email;
+        usuarioDB.role= body.role;
+
+        usuarioDB.save((err, usuarioGuardado)=>{
+            if(err) return res.status(400).json({ok:false, mensaje: 'Error al actualizar usuario', errors: err})
+            usuarioGuardado.password = ':)'
+            res.status(200).json({
+                ok:true,
+                Usuario: usuarioGuardado
+            });
+        })
+    })
+});
+
+app.delete('/:id', middAutenticacion.verificaToken, (req, res)=>{
+    var id = req.params.id;
+    Usuario.findByIdAndRemove(id, (err, usuarioDB)=>{
+        if(err) return res.status(500).json({ok:false, mensaje: 'Error al borrar usuario', errors: err})
+        if(!usuarioDB) return res.status(400).json({ok:false, mensaje: 'No existe un usuario con ese ID', errors: err})
+        res.status(200).json({
+            ok:true,
+            Usuario: usuarioDB
+        });
+    })
+})
+
+module.exports = app;
